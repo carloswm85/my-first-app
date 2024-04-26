beaaf9420c8b5daf9c2a0ed497b7c7e25b858b73
Carlos W. Mercado <carlos.washington.mercado@gmail.com>
2024-04-26 18:30:47 -0300

    added styles and starting project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
 
 import { AppComponent } from './app.component';
 
@@ -10,8 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
